2c647431a68311780a9171d19fe6c6cabffea7d9
Arthur Richards <awjrichards@users.mediawiki.org>
2010-10-25 03:30:53 +0000

    Fixed trxn_id -> tx_id

--- paypal/IPN/PaypalIPNListener.php
+++ paypal/IPN/PaypalIPNListener.php
@@ -261,14 +261,14 @@ class PaypalIPNProcessor {
 			// send email to configured recipients notifying them of the PayPal verification failure
 			if ( $this->email_recipients && count( $this->email_recipients )) {
 				$to = implode( ", ", $this->email_recipients );
-				$subject = "IPN Listener verification failure for message " . $this->trxn_id;
+				$subject = "IPN Listener verification failure for message " . $this->tx_id;
 				$msg = "Greetings!\n\n";
 				$msg .= "You are receiving this message because a transaction that was psoted to the ";
 				$msg .= "PayPal IPN listener failed PayPal verification.  The contents of the original ";
 				$msg .= "payload are below:\n\n";
 				$msg .= print_r( $post_data, true );
 				$msg .= "\n\n";
-				$msg .= "The IPN listener-assigned trxn id for this transaction is: " . $this->trxn_id . "\n\n";
+				$msg .= "The IPN listener-assigned trxn id for this transaction is: " . $this->tx_id . "\n\n";
 				$msg .= "Good luck figuring out wtf happened!\n\n";
 				$msg .= "Love always,\n";
 				$msg .= "Your faithful IPN listener";
