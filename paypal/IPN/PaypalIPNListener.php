9677dea66b5a1942ea3af2b77e696b3c001a5656
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-12-21 21:58:45 +0000

    fixing for PayPal weirdness

--- paypal/IPN/PaypalIPNListener.php
+++ paypal/IPN/PaypalIPNListener.php
@@ -311,8 +311,8 @@ class PaypalIPNProcessor {
 		$contribution['anonymous'] = $tracking_data['anonymous'];
 		$contribution['comment'] = $tracking_data['note'];
 
-		$contribution['size'] = $post_data['option_selection0'];
-		$contribution['premium_language'] = $post_data['option_selection1'];
+		$contribution['size'] = $post_data['option_selection1'];
+		$contribution['premium_language'] = $post_data['option_selection2'];
 		$contribution['email'] = $post_data['payer_email'];
 		$contribution['first_name'] = $post_data['first_name'];
 		$contribution['last_name'] = $post_data['last_name'];
