d5c37cf01dcf41fa1192fd797f7653c0c4df73ed
Jeremy Postlethwaite <jpostlethwaite@users.mediawiki.org>
2011-12-14 20:26:49 +0000

    Added helper methods: getPostDataForGlobalCollect(), getPostDataForGlobalCollectWithEmptyOrderId(), and getPostDataForGlobalCollectWithOutOrderId() to facilitate unit testing.

--- tests/QueueHandlingTestCase.php
+++ tests/QueueHandlingTestCase.php
@@ -26,4 +26,94 @@ require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'TestHelper.php';
  */
 abstract class QueueHandlingTestCase extends PHPUnit_Framework_TestCase
 {
+	
+	/**
+	 * getPostDataForGlobalCollect
+	 *
+	 * This is used to generate posted form data from the PSC Listener
+	 *
+	 * Everything should be returned as strings, in the array, since that is how
+	 * they will be sent by the form.
+	 *
+	 * Anything set in $data will be converted to a string.
+	 *
+	 * If a value is null, in $data, it will be removed from $return.
+	 *
+	 * @param array $data	Anything set in this array will be returned.
+	 *
+	 * @return array
+	 */
+	public function getPostDataForGlobalCollect( $data = array()) {
+
+		// Everything should be returned as strings since that is how they will be sent by the form
+		$return = array(
+			'MERCHANTID'		=> '9990',
+			'ORDERID'			=> '23',
+			'EFFORTID'			=> '1',
+			'ATTEMPTID'			=> '1',
+			'AMOUNT'			=> '100',
+			'CURRENCYCODE'		=> 'EUR',
+			'REFERENCE'			=> '20070406GC19',
+			'PAYMENTREFERENCE'	=> '',
+			'PAYMENTPRODUCTID'	=> '1',
+			'PAYMENTMETHODID'	=> '8',
+			'STATUSID'			=> '800',
+			'STATUSDATE'		=> '20070406170059',
+			'RECEIVEDDATE'		=> '20070406170057',
+		);
+
+		if ( is_array( $data ) ) {
+
+			foreach( $data as $key => $value ) {
+				
+				// Remove values from return if $value is null.
+				if ( is_null( $value ) ) {
+				
+					if ( isset( $return[ $key ] ) ) {
+						
+						unset( $return[ $key ] );
+					}
+				}
+				else {
+					$return[ $key ] = (string) $value;
+				}
+				
+				
+			}
+		}
+		
+		return $return;
+	}
+	
+	/**
+	 * getPostDataForGlobalCollectWithEmptyOrderId
+	 *
+ 	 * @see QueueHandlingTestCase::getPostDataForGlobalCollect
+	 *
+	 * @param array $data	Anything set in this array will be returned.
+	 *
+	 * @return array
+	 */
+	public function getPostDataForGlobalCollectWithEmptyOrderId( $data = array()) {
+
+		$data['ORDERID'] = '';
+		
+		return $this->getPostDataForGlobalCollect( $data );
+	}
+	
+	/**
+	 * getPostDataForGlobalCollectWithOutOrderId
+	 *
+ 	 * @see QueueHandlingTestCase::getPostDataForGlobalCollect
+	 *
+	 * @param array $data	Anything set in this array will be returned.
+	 *
+	 * @return array
+	 */
+	public function getPostDataForGlobalCollectWithOutOrderId( $data = array()) {
+
+		$data['ORDERID'] = null;
+		
+		return $this->getPostDataForGlobalCollect( $data );
+	}
 }
