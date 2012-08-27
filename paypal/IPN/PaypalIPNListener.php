f11454a4f7b7e4df3ad5e5fcd436987e90419f0b
Adam Wight <adamw@users.mediawiki.org>
2012-08-27 17:31:04 +0000

    Paypal's endpoint sometimes redirects to the home page. Don't follow the redirect, just interpret as an error.

--- paypal/IPN/PaypalIPNListener.php
+++ paypal/IPN/PaypalIPNListener.php
@@ -380,7 +380,7 @@ class PaypalIPNProcessor {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HEADER, 0); 
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 		    
 		if ($vars !== NULL) {
