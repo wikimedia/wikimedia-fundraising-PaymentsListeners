3a615f55668f471f4ab0b3c04c4ccaf252786e3b
Adam Roses Wight <awight@wikimedia.org>
2014-02-28 14:32:20 -0800

    put stupid entrypoint scripts into source control
    Change-Id: I0d6b7a3feff8f263e1b90f3b96ad0a7be71c55f0

---
+++ legacy-paypal/IPNListener_Standalone.php
@@ -0,0 +1,20 @@
+<?php
+
+$conf_path = '/etc/fundraising/legacy_paypal_config.php';
+
+require_once( $conf_path );
+
+$config['output_handle'] = fopen( $config['log_file'], 'a' );
+require_once( $config['script_path'] );
+
+// instantaite the listener with our config options
+$listener = new PaypalIPNProcessor( $config );
+
+// pass some data to the listner, usually this will be posted from PayPal's IPN
+$listener->execute( $_POST );
+
+// shutdown the listener
+unset( $listener );
+
+// cleanly close the file pointer for output
+close( $config['output_handle'] );
