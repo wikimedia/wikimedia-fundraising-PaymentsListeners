21647c8f4b781b74ae2dc4377334410b4eed7e3c
Adam Roses Wight <awight@wikimedia.org>
2016-09-26 10:20:51 -0700

    Redirect to new listener implementation
    Change-Id: I87566cfda661dcee92831d8365943cee33f4db28
    TODO: rewrite nginx config instead.

--- legacy-paypal/IPNListener_Recurring.php
+++ legacy-paypal/IPNListener_Recurring.php
@@ -1,20 +1,3 @@
 <?php
 
-$conf_path = '/etc/fundraising/legacy_paypal_config.php';
-
-require_once( $conf_path );
-
-$config['output_handle'] = fopen( $config['recurring_log_file'], 'a' );
-require_once( $config['recurring_script_path'] );
-
-// instantaite the listener with our config options
-$listener = new PaypalRecurringIPNProcessor( $config );
-
-// pass some data to the listner, usually this will be posted from PayPal's IPN
-$listener->execute( $_POST );
-
-// shutdown the listener
-unset( $listener );
-
-// cleanly close the file pointer for output
-fclose( $output_handle );
+header( 'Location: https://payments-listener.wikimedia.org/paypal2' );
--- legacy-paypal/IPNListener_Standalone.php
+++ legacy-paypal/IPNListener_Standalone.php
@@ -1,20 +1,3 @@
 <?php
 
-$conf_path = '/etc/fundraising/legacy_paypal_config.php';
-
-require_once( $conf_path );
-
-$config['output_handle'] = fopen( $config['log_file'], 'a' );
-require_once( $config['script_path'] );
-
-// instantaite the listener with our config options
-$listener = new PaypalIPNProcessor( $config );
-
-// pass some data to the listner, usually this will be posted from PayPal's IPN
-$listener->execute( $_POST );
-
-// shutdown the listener
-unset( $listener );
-
-// cleanly close the file pointer for output
-fclose( $config['output_handle'] );
+header( 'Location: https://payments-listener.wikimedia.org/paypal2' );
